95458fff7bc00b191449ad6a1762bd8202ed82cf
LukasBlohm <l.blohm98@gmail.com>
2023-09-17 09:56:42 +0200

    Project setup
    golem framework and first iteration of the vocab module

---
+++ R/mod_vocab.R
@@ -0,0 +1,60 @@
+#' vocab UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd
+#'
+#' @importFrom shiny NS tagList
+mod_vocab_ui <- function(id){
+  ns <- NS(id)
+
+  tagList(
+    selectInput(ns("language"), "Choose Language:", choices = c("EN", "FR")),
+    radioButtons(ns("word_source"), "Word Source:", choices = c("Random", "Choose")),
+    uiOutput(ns("word_display")),
+    textInput(ns("guess"), "Your Guess:"),
+    actionButton(ns("submit"), "Submit"),
+    textOutput(ns("feedback"))
+  )
+}
+
+#' vocab Server Functions
+#'
+#' @noRd
+mod_vocab_server <- function(id){
+  moduleServer( id, function(input, output, session){
+    ns <- session$ns
+
+    # Read the vocab data
+    vocab_data <- reactive({
+      read.csv("vocab_1000.csv")
+    })
+
+    # Randomly sample or let the user choose a word
+    random_word <- reactive({
+      if (input$word_source == "Random") {
+        sample(vocab_data()[, input$language], 1)
+      } else {
+        # Code to let user choose a word
+      }
+    })
+
+    # Display the word to the user
+    output$word_display <- renderUI({
+      random_word()
+    })
+
+    # Provide feedback on the user's guess
+    observeEvent(input$submit, {
+      real_translation <- vocab_data()[vocab_data()[, input$language] == random_word(), "FR"]
+      if (input$guess == real_translation) {
+        output$feedback <- renderText({"Correct!"})
+      } else {
+        output$feedback <- renderText({"Incorrect. Keep practicing!"})
+      }
+    })
+  })
+}
+
