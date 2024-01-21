a1710fb4a052c782f943c8d89c17bd8f21aff366
LukasBlohm <l.blohm98@gmail.com>
2024-01-21 15:20:41 +0100

    Support predefined port from Heroku

--- app.R
+++ app.R
@@ -1,2 +1,7 @@
 pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
 options("golem.app.prod" = TRUE)
+
+# Set port according to environment variable (from Heroku) if there is one.
+port <- Sys.getenv("PORT")
+if (port == "") port <- 3838
+options('shiny.port' = as.numeric(port), shiny.host = '0.0.0.0')
